6eaa42f5a7760b8dcbb2f1aba2d3cf00a80deccd
mbm <daffodilss19@gmail.com>
2026-04-26 15:58:35 +0530

    fix: dynamic IAM naming

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -1,5 +1,11 @@
+# Generate unique suffix
+resource "random_id" "suffix" {
+  byte_length = 2
+}
+
+# IAM Role
 resource "aws_iam_role" "ec2_role" {
-  name = "ec2-secure-role"
+  name = "ec2-secure-role-${random_id.suffix.hex}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -13,20 +19,20 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
+# Attach SSM Policy
 resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 
+# Attach Secrets Manager Policy
 resource "aws_iam_role_policy_attachment" "secrets" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
 
+# Instance Profile (also dynamic)
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-profile"
+  name = "ec2-profile-${random_id.suffix.hex}"
   role = aws_iam_role.ec2_role.name
 }
-
-
-
